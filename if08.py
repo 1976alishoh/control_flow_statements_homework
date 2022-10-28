3504774d48381cd346da8c644b06d601e4604aff
1976alishoh <106156697+iqrobocreator@users.noreply.github.com>
2022-10-28 09:43:38 +0300

    08 done

--- if08.py
+++ if08.py
@@ -11,13 +11,13 @@ def main(a):
     Returns:
         string: the message to print
     """
-    if 9 < a < 100 and a % 2 == 1 and -100 < a < -9:
+    if 9 < a < 100 and a % 2 == 1 :
         return  "two-digit odd number"
-    if 9 < a < 100 and a % 2 == 0 and -100 < a < -9:
+    if 9 < a < 100 and a % 2 == 0 :
         return "two-digit even number"
-    if 99 < a < 1000 and a % 2 == 1 and -1000 < a < -99:
+    if 99 < a < 1000 and a % 2 == 1 :
         return  "three-digit odd number"
-    if 99 < a < 1000 and a % 2 == 0 and -1000 < a < -99:
+    if 99 < a < 1000 and a % 2 == 0 :
         return "three-digit even number"
 print(main(57))
 print(main(-242))
